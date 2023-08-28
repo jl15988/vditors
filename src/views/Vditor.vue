3639c076f66a620c83fe1690fa81e8211df2ccb5
lijianlong <jl15988@qq.com>
2023-08-28 16:06:57 +0800

    初始化项目

---
+++ src/views/Vditor.vue
@@ -0,0 +1,63 @@
+<template>
+  <div id="vditor"></div>
+</template>
+
+<script setup lang="ts">
+import { onMounted, ref } from 'vue'
+import Vditor from 'vditor'
+import 'vditor/dist/index.css'
+
+const vditor = ref<Vditor | null>(null)
+
+const toolbars = [
+  'headings',
+  'bold',
+  'italic',
+  'strike',
+  '|',
+  'line',
+  'quote',
+  'list',
+  'ordered-list',
+  'check',
+  'outdent',
+  'indent',
+  'code',
+  'inline-code',
+  'insert-after',
+  'insert-before',
+  'undo',
+  'redo',
+  'upload',
+  'link',
+  'table',
+  'record',
+  'edit-mode',
+  'both',
+  'preview',
+  'fullscreen',
+  'outline',
+  'code-theme',
+  'content-theme',
+  'export',
+  'devtools',
+  'br'].map(item => {
+  return {
+    name: item,
+    tipPosition: 's'
+  }
+})
+
+onMounted(() => {
+  vditor.value = new Vditor('vditor', {
+    placeholder: '在此处输入内容...',
+    toolbar: toolbars
+  })
+})
+</script>
+
+<style scoped>
+#vditor {
+  min-height: 100vh;
+}
+</style>
